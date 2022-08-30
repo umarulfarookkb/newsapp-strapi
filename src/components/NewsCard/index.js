82448e5554bc6c5dd08478dfabcf77cd38ae5225
Umarul Farook <umarulfarookkb@gmail.com>
2022-08-30 14:23:58 +0530

    image size change

--- src/components/NewsCard/index.js
+++ src/components/NewsCard/index.js
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 import "./NewsCard.css";
-export default function NewsCard({ newsItem,id }) {
+export default function NewsCard({ newsItem, id }) {
   const { title, body, imageUrl } = newsItem;
   const synopsis = body.slice(0, 150);
   return (
     <Link to={`/newsview/${id} `}>
       <div className="newscard">
         <div
-          className="newscardimg"
-          style={{ backgroundImage: `url(${imageUrl})` }}
-        ></div>
+        // style={{ backgroundImage: `url(${imageUrl})` }}
+        >
+          <img src={imageUrl} className="newscardimg" />
+        </div>
         <div>
           <div className="newscardtitle">
             <h1>{title}</h1>
